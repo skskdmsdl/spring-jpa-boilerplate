7a043fee896791ee2cbfaa693bb5faa5677e4256
skskdmsdl <skskdmsdl@gmail.com>
2022-04-18 10:32:23 +0900

    spring jpa boilerplate

---
+++ src/main/java/com/sample/spring/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.sample.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+	public String hello() {
+		return "main";
+	}
+
+}
